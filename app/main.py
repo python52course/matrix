e75b396ff777ac5e90c999cd8d2af444a486b594
Aleksandr Smelik <suntrope55@gmail.com>
2024-09-07 16:52:03 +0600

    feat: add base endpont ping server, and tests for it

--- app/main.py
+++ app/main.py
@@ -3,6 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route("/ping")
+def ping() -> dict:
+    """
+    A simple endpoint to check if the application is running.
+
+    Returns:
+        dict: A dictionary with the key "ping" and the value "pong".
+    """
+    return {"ping": "pong"}
